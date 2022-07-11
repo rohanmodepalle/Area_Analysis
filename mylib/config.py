f986c5ba6cf4f1f38c00ed02ed25769229a065e3
rohanmodepalle <rohan.modepalle@students.iiit.ac.in>
2022-07-11 21:23:00 +0530

    commit

--- mylib/config.py
+++ mylib/config.py
@@ -2,7 +2,7 @@
 # Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
 #url = 'rtsp://codonsoft:rohanchowdary@192.168.0.131:554/stream1'
 # url = [0, 'rtsp://codonsoft:rohanchowdary@192.168.0.106:554/stream1']
-url = ['videos/example_01.mp4','videos/example_02.mp4']
+url = ['videos/example_01.mp4']
 threshold_time=2
 # Auto run/Schedule the software to run at your desired time
 Scheduler = False
